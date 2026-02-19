ac689c1cfe19b71656b4a1655f2514268abf3ddf
naichiehlin <daniellin456@gmail.com>
2026-02-19 22:01:53 +0800

    [Fix] Update Discord link to email with subject and remove target blank

--- src/data/Constants.tsx
+++ src/data/Constants.tsx
@@ -63,7 +63,7 @@ export const CONSTANTS: ConstantsType = {
   },
   links: {
     spice1: "https://www.astr.tohoku.ac.jp/~tomida/spice/",
-    discord: "mailto:spice2.workshop@gmail.com",
+    discord: "mailto:spice2.workshop@gmail.com?subject=Join%20the%20SPiCE%20discord%20server",
     slack: "https://slack.com/example",
     cloudDrive: "#",
     googleCalendar: "https://calendar.google.com/calendar/embed?src=example",
